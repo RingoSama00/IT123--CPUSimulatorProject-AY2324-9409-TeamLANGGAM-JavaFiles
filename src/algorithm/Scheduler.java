337552ae40eda4c076898062e8de4d3b4c756336
RingoSama <ajsarmiento2004@gmail.com>
2024-06-19 19:37:16 +0800

    IT WORKS

--- src/algorithm/Scheduler.java
+++ src/algorithm/Scheduler.java
@@ -59,53 +59,84 @@ public class Scheduler {
 
         // processor one algorithms
         if (processor1.getProcess() != null) {
-
-            // system queue (round robin algorithm)
             if (processor1.getProcess().getType().equals("system")) {
                 if (processor1.getProcessRunningTime() >= SYSTEM_QUEUE_RR_QUANTUM) {
                     Process p = processor1.getProcess();
                     p.demoteType();
                     interactiveQueue.add(p);
                     Process shortestProcess = findShortestProcess(interactiveQueue);
-                    systemQueue.remove(shortestProcess);
+                    processor1.setProcess(shortestProcess); // no need for nullable checks
+                    interactiveQueue.remove(shortestProcess);
                 }
-
-                // interactive queue (shortest time remaining first algorithm)
             } else if (processor1.getProcess().getType().equals("interactive")) {
-
-                // demotes any remaining processes from previous queue
                 if (!systemQueue.isEmpty()) {
                     Process p = processor1.getProcess();
                     processor1.setProcess(systemQueue.remove(0));
                     p.demoteType();
+                    batchQueue.add(p);
                 }
-
-                // find the shortest process and compare remaining burst time
                 Process shortestProcess = findShortestProcess(interactiveQueue);
                 if (shortestProcess != null
                         && processor1.getProcess() != shortestProcess
-                        && processor1.getProcessRunningTime() < shortestProcess.getArrival()) {
-                    Process p = processor2.getProcess();
+                        && processor1.getProcess().getBurst() > shortestProcess.getBurst()) {
+                    Process p = processor1.getProcess();
+                    interactiveQueue.remove(shortestProcess);
+                    processor1.setProcess(shortestProcess);
                     p.demoteType();
                     batchQueue.add(p);
-
-                    processor1.setProcess(shortestProcess);
-                    processor2.setProcess(shortestProcess);
-                    interactiveQueue.remove(shortestProcess);
-
                 }
-
-                // batch queue (first come first serve algorithm)
             } else if (processor1.getProcess().getType().equals("batch")) {
-                // checks both queues for remaining processes and transfers it too batch queue
                 if (!systemQueue.isEmpty()) {
                     Process p = processor1.getProcess();
                     processor1.setProcess(systemQueue.remove(0));
                     p.demoteType();
+                    batchQueue.add(0, p);
                 }
                 if (!interactiveQueue.isEmpty()) {
                     Process p = processor1.getProcess();
-                    processor2.setProcess(p);
+                    processor1.setProcess(interactiveQueue.remove(0));
+                    p.demoteType();
+                    batchQueue.add(0, p);
+                }
+            }
+        }
+        if (processor2.getProcess() != null) {
+            if (processor2.getProcess().getType().equals("system")) {
+                if (processor2.getProcessRunningTime() >= SYSTEM_QUEUE_RR_QUANTUM) {
+                    Process p = processor2.getProcess();
+                    p.demoteType();
+                    interactiveQueue.add(p);
+                    Process shortestProcess = findShortestProcess(interactiveQueue);
+                    processor2.setProcess(shortestProcess); // no need for nullable checks
+                    interactiveQueue.remove(shortestProcess);
+                }
+            } else if (processor2.getProcess().getType().equals("interactive")) {
+                if (!systemQueue.isEmpty()) {
+                    Process p = processor2.getProcess();
+                    processor2.setProcess(systemQueue.remove(0));
+                    p.demoteType();
+                    batchQueue.add(p);
+                }
+                Process shortestProcess = findShortestProcess(interactiveQueue);
+                if (shortestProcess != null
+                        && processor2.getProcess() != shortestProcess
+                        && processor2.getProcess().getBurst() > shortestProcess.getBurst()) {
+                    Process p = processor2.getProcess();
+                    interactiveQueue.remove(shortestProcess);
+                    processor2.setProcess(shortestProcess);
+                    p.demoteType();
+                    batchQueue.add(p);
+                }
+            } else if (processor2.getProcess().getType().equals("batch")) {
+                if (!systemQueue.isEmpty()) {
+                    Process p = processor2.getProcess();
+                    processor2.setProcess(systemQueue.remove(0));
+                    p.demoteType();
+                    batchQueue.add(0, p);
+                }
+                if (!interactiveQueue.isEmpty()) {
+                    Process p = processor2.getProcess();
+                    processor2.setProcess(interactiveQueue.remove(0));
                     p.demoteType();
                     batchQueue.add(0, p);
                 }
@@ -116,13 +147,19 @@ public class Scheduler {
         if (processor1.getProcess() == null) {
             if (!systemQueue.isEmpty()) {
                 processor1.setProcess(systemQueue.remove(0));
+            } else if (!interactiveQueue.isEmpty()) {
+                processor1.setProcess(interactiveQueue.remove(0));
             } else if (!batchQueue.isEmpty()) {
                 processor1.setProcess(batchQueue.remove(0));
             }
         }
         if (processor2.getProcess() == null) {
-            if (!interactiveQueue.isEmpty()) {
+            if (!systemQueue.isEmpty()) {
+                processor2.setProcess(systemQueue.remove(0));
+            } else if (!interactiveQueue.isEmpty()) {
                 processor2.setProcess(interactiveQueue.remove(0));
+            } else if (!batchQueue.isEmpty()) {
+                processor2.setProcess(batchQueue.remove(0));
             }
         }
 
